d18d8b278bb589cd27595fcd1486abaffd85ce16
sebastia-mejia-18 <juansebastianmejiaagudelo73@gmail.com>
2024-09-16 12:10:10 -0500

    javaScript

---
+++ condicionales/script.js
@@ -0,0 +1,55 @@
+/*
+if-> palabra reservada de condicional simple
+()-> exprecion booleada a evaluar
+{}-> bloque de codigo
+else -> es la palabra de condicinal compuesto
+*/
+
+//if(5 !='5'){
+//console.log('condicional simple')
+//}else if(5 > 5){
+//console.log('condicional compuesto')
+//}else{
+// console.log('condicional anidado')
+//}
+
+
+//if(5 >= 5) {
+ // if (5 !== '5'){
+  //    if(5>4){
+ //       console.log('se cumple la condicion anidada')
+  // }
+ // }
+//}
+
+//if(5 >= 5 && 5 !== '5' && 5 > 4){
+// console.log('$se cumple la condicion anidada$')//
+//}
+
+//if (true && true && true){
+// console.log('!se cumple la condicion anidada!')
+//}
+
+
+
+
+//let opcion = prompt (' SELECIONE: 1 caso 1 --  2 - caso 2'  )
+//switch(Number(opcion)){
+//case 1:
+ //     console.log('este es el caso 1')
+ //     break;
+ 
+ //     case '2':
+ //       console.log('Este es el caso 2')
+ //       break;
+//}
+//console.log(typeof(opcion))
+
+//
+//let opcion =Number( prompt (' SELECIONE: 1 caso 1 --  2 - caso 2'))
+//console.log(opcion)
+// 
+//
+ console.log(isNaN(numeroUNO- Number(numeroDOS)))
+//
+//
